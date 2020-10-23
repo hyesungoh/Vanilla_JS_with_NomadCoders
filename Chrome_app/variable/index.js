842756066c0857563f01fced7a448702eb89e5f4
hyesungoh <haesungoh414@gmail.com>
2020-10-24 03:13:15 +0900

    chromeapp - 1.7

--- Chrome_app/variable/index.js
+++ Chrome_app/variable/index.js
@@ -1,4 +1,10 @@
-let a = 221
-let b = a - 5
-a = 4
-console.log('b는', b, 'a는', a)
+let a = 221;
+let b = a - 5;
+a = 4;
+console.log('b는', b, 'a는', a);
+
+
+var c = 111 // 원래는 var만 쓸 수 있었다고 함
+let d = 222
+const e = 100 // const는 상수 (안정적이란 뜻) 값이 변하지 않음
+console.log(c, d, e);
