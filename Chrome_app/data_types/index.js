4b56a357dbee4f1d40fc01e902503d6c2cb2200b
hyesungoh <haesungoh414@gmail.com>
2020-10-24 03:29:55 +0900

    chromeapp - 1.8

---
+++ Chrome_app/data_types/index.js
@@ -0,0 +1,26 @@
+// one-line comment
+
+/*
+multi
+line
+comment
+*/
+
+// 변수 선언 시 const로 선언 후 필요할 때 let으로 비꾸자
+const number = 10;
+
+// string
+const name = 'hyesungoh 😁'; // 이모티콘도 문자열로 가능하넹 ㅋㅋ
+console.log(name);
+
+// boolean
+const gender = true; // or false와 같이 모두 소문자
+console.log(gender);
+
+// number
+const age = 23;
+console.log(age);
+
+// float
+const weight = 88.5;
+console.log(weight);
