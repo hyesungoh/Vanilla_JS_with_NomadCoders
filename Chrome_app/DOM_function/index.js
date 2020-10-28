0cf6255b3b7bdd56f0d939f080f1c01ec5f57f5b
hyesungoh <haesungoh414@gmail.com>
2020-10-29 02:58:36 +0900

    chromeapp - 2.2, 2.3

---
+++ Chrome_app/DOM_function/index.js
@@ -0,0 +1,10 @@
+// const title = document.getElementById("title");
+
+// 첫번째로 나오는 값을 반환
+const title = document.querySelector("#title");
+
+title.innerHTML = "Hi this is from JS";
+title.style.color = "blue";
+// console.dir(title)
+
+document.title = "Im own you now";
