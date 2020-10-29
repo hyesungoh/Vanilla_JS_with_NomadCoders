cb51c5c7cad031761ad576ec675934869c222c4e
hyesungoh <haesungoh414@gmail.com>
2020-10-30 04:33:28 +0900

    chromeapp - 3.0, 3.1

---
+++ Chrome_app/ohmentum/clock.js
@@ -0,0 +1,25 @@
+const clockContainer = document.querySelector(".js-clock"),
+      clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${
+    minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+}
+
+
+// setInterver(fn, time)
+// time 마다 fn 호출
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
