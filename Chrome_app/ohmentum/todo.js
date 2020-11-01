1d0ffef70953908033d9a460215fa38dea47d148
hyesungoh <haesungoh414@gmail.com>
2020-11-02 00:08:29 +0900

    chromeapp - 3.5

--- Chrome_app/ohmentum/todo.js
+++ Chrome_app/ohmentum/todo.js
@@ -2,7 +2,14 @@ const toDoForm = document.querySelector(".js-toDoForm"),
       toDoInput = toDoForm.querySelector("input"),
       toDoList = document.querySelector(".js-toDoList");
 
+const toDos = []
+
 const TODOS_LS = "toDos"
+
+function saveToDos(){
+  // JSON.stringify는 모든 객체들을 문자열 형태로 만들어줌
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 
 function paintToDo(text) {
   const li = document.createElement("li");
@@ -10,10 +17,20 @@ function paintToDo(text) {
   span.innerText = text;
   const delBtn = document.createElement("button");
   delBtn.innerText = "X";
+  const newId = toDos.length + 1;
 
   li.appendChild(span);
   li.appendChild(delBtn);
+  li.id = newId;
   toDoList.appendChild(li);
+
+  toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+
+  saveToDos();
 }
 
 function handlerSubmit(event){
@@ -24,9 +41,19 @@ function handlerSubmit(event){
 }
 
 function loadToDos(){
-  const toDos = localStorage.getItem(TODOS_LS);
-  if(toDos !== null) {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if(loadedToDos !== null) {
+    // JSON = JavaScript Object Notation
+    // JSON.parse를 이용하여 문자열을 객체로
+    const parsedToDos = JSON.parse(loadedToDos);
 
+    // array.forEach(fnc) = array안에 있는 모든 요소들을
+    // fnc 함수에 적용. 괄호안에 함수를 직접 작성 가능
+    // python의 map과 비슷한듯
+
+    parsedToDos.forEach(function(toDo){
+      paintToDo(toDo.text);
+    })
   }
 }
 
