f152c3d533282d2758dc92ba1b1de97f9ec1b45a
hyesungoh <haesungoh414@gmail.com>
2020-11-05 01:43:00 +0900

    clock changing with animate

--- Chrome_app/ohmentum/clock.js
+++ Chrome_app/ohmentum/clock.js
@@ -1,5 +1,8 @@
 const clockContainer = document.querySelector(".js-clock"),
-      clockTitle = clockContainer.querySelector("h1");
+      clockHours = clockContainer.querySelector(".js-hours"),
+      clockMinutes = clockContainer.querySelector(".js-minutes"),
+      clockSeconds = clockContainer.querySelector(".js-seconds");
+      // clockHours = clockContainer.querySelector("h1");
 
 function getTime() {
   const date = new Date();
@@ -7,10 +10,66 @@ function getTime() {
   const hours = date.getHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  clockTitle.innerHTML = `${hours < 10 ? `0${hours}` : hours}:${
-    minutes < 10 ? `0${minutes}` : minutes}:${
-    seconds < 10 ? `0${seconds}` : seconds
-  }`;
+
+  clockHours.innerHTML = hours < 10 ? `0${hours}` : hours;
+  clockMinutes.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  clockSeconds.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+}
+
+function getHours() {
+  const date = new Date();
+  const hours = date.getHours();
+
+  clockHours.innerHTML = hours < 10 ? `0${hours}` : hours;
+  clockHours.animate([
+    {opacity: 0,
+     easing: 'ease-in-out'},
+    {opacity: 1,
+     easing: 'ease-in-out'}
+  ], {
+    duration: 1200,
+    iteration: Infinity
+  });
+}
+
+function getMinutes() {
+  const date = new Date();
+  const minutes = date.getMinutes();
+
+  clockMinutes.innerHTML = minutes < 10 ? `0${minutes}` : minutes;
+  clockMinutes.animate([
+    {opacity: 0,
+     easing: 'ease-in-out'},
+    {opacity: 1,
+     easing: 'ease-in-out'}
+  ], {
+    duration: 1200,
+    iteration: Infinity
+  });
+
+  if (minutes == 0) {
+    getHours();
+  }
+}
+
+function getSeconds() {
+  const date = new Date();
+  const seconds = date.getSeconds();
+
+  clockSeconds.innerHTML = seconds < 10 ? `0${seconds}` : seconds;
+  clockSeconds.animate([
+    {opacity: 0,
+     easing: 'ease-in-out'},
+    {opacity: 1,
+     easing: 'ease-in-out'}
+  ], {
+    duration: 1200,
+    iteration: Infinity
+  });
+
+  if (seconds == 0){
+    getMinutes();
+  }
 }
 
 
@@ -19,7 +78,8 @@ function getTime() {
 
 function init() {
   getTime();
-  setInterval(getTime, 1000);
+  setInterval(getSeconds, 1000);
+  // setInterval(getTime, 1000);
 }
 
 init();
